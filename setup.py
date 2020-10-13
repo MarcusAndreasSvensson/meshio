35a80c0599ab8f3e9bb109da74e53c8e2e11c398
Marcus Svensson <marsven95@gmail.com>
2020-10-13 17:38:52 +0200

    changed setup

--- setup.py
+++ setup.py
@@ -19,5 +19,5 @@ setuptools.setup(
         "Operating System :: OS Independent",
         "Development Status :: 2 - Pre-Alpha",
     ],
-    python_requires=">=3.7",
+    python_requires=">=3.6",
 )
